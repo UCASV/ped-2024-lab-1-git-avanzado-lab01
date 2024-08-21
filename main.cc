6e7880d489d6ab91a6ef57ea59f98fc6b33e56f5
AleXxRecinos1306 <00379423@uca.edu.sv>
2024-08-21 10:32:38 -0600

    feat: menu principal

---
+++ main.cc
@@ -0,0 +1,40 @@
+#include<iostream>
+
+int main (){
+    int opc = 0;
+    float number1 = 0;
+    float number2 = 0;
+    float number3 = 0;
+
+    std::cout<<"que operacion desea realizar:\n"<<"1-suma.\n"<<"2-resta."<<"3-multiplicacion."<<"4-division."<<std::endl;
+    std::cin>>opc;
+
+
+    
+
+    switch (opc)
+    {
+        case 1:
+            std::cout<<"suma";
+            break:
+
+        case 2:
+            std::cout<<"resta";
+            break:
+        case 3:
+            std::cout<<"multiplicacion";
+            break:
+        case 4:
+            std::<<cout"division";
+            break:
+
+    }
+
+    
+
+
+
+
+return 0;
+
+}
