97b25a9f895d77cff9cca298fd102c228a679098
IgnacioPeralta00 <00196321@uca.edu.sv>
2024-08-21 12:05:56 -0600

    feat: Se añadio funcion suma

--- main.cc
+++ main.cc
@@ -1,37 +1,29 @@
-#include<iostream>
+#include <iostream>
 
-int main (){
+int main()
+{
     int opc = 0;
     float number1 = 0;
     float number2 = 0;
     float number3 = 0;
 
-    std::cout<<"que operacion desea realizar:\n"<<"1-suma.\n"<<"2-resta."<<"3-multiplicacion."<<"4-division."<<std::endl;
-    std::cin>>opc;
-
-
-    
-
+    std::cout << "que operacion desea realizar:\n"
+              << "1-suma.\n"
+              << "2-resta." << std::endl;
+    std::cin >> opc;
     switch (opc)
     {
-        case 1:
-            std::cout<<"suma";
-            number3 = number1 + number2;
-            std:: cout << number3;
-            break;
+    case 1:
+        std::cout << "la suma es: \n";
+        number3 = number1 + number2;
+        std::cout << number3;
+        break;
 
-        case 2:
-            std::cout<<"resta";
-            break;
-        
-
+    case 2:
+        std::cout << "la resta es: \n";
+        number3 = number1 - number2;
+        std::cout << number3;
+        break;
     }
-
-    
-
-
-
-
-return 0;
-
+    return 0;
 }
